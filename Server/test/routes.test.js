efc196dc9b326dba40e27a29c99693829d14f90f
JaiOwl <jaiowl@yahoo.co.uk>
2018-04-19 21:05:04 +0100

    Added test for api/version

--- Server/test/routes.test.js
+++ Server/test/routes.test.js
@@ -1,28 +1,45 @@
 import request from 'supertest';
 import app from '../src/app.js';
+import packageJson from './../package.json';
 
-describe('GET /', () => {
-  it('should render properly', async () => {
-    await request(app).get('/').expect(200);
-  });
-});
+describe('GET /', 
+  () => {
+    it('should render properly', 
+      async () => {
+        await request(app).get('/').expect(200);
+      }
+    );
+  }
+);
 
-describe('GET /list', () => {
-  it('should render properly with valid parameters', async () => {
-    await request(app)
-      .get('/list')
-      .query({ title: 'List title' })
-      .expect(200);
-  });
+describe('GET /api/version', 
+  () => {
+    it('should return the service name and version', 
+      async () => {
+        await request(app)
+          .get('/api/version')
+          .set('Accept', 'application/json')
+          .expect(200)
+          .then(
+            (response) => {
+              expect(response.body).not.toBeNull();
+              expect(response.body).not.toBeUndefined();
+              expect(response.body.service).toEqual(`${packageJson.name}`);
+              expect(response.body.version).toEqual(`${packageJson.version}`);
+            }
+          );
+      }
+    );
+  }
+);
 
-  it('should error without a valid parameter', async () => {
-    await request(app).get('/list').expect(500);
-  });
-});
-
-describe('GET /404', () => {
-  it('should return 404 for non-existent URLs', async () => {
-    await request(app).get('/404').expect(404);
-    await request(app).get('/notfound').expect(404);
-  });
-});
+describe('GET /404', 
+  () => {
+    it('should return 404 for non-existent URLs', 
+      async () => {
+        await request(app).get('/404').expect(404);
+        await request(app).get('/notfound').expect(404);
+      }
+    );
+  }
+);
